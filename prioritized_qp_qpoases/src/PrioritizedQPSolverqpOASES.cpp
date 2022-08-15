74ebdc3a6ecc9a6b937a071a54d464f1de8f9157
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2022-08-15 18:08:48 +0900

    fix bug

--- prioritized_qp_qpoases/src/PrioritizedQPSolverqpOASES.cpp
+++ prioritized_qp_qpoases/src/PrioritizedQPSolverqpOASES.cpp
@@ -1,4 +1,5 @@
 #include <prioritized_qp_qpoases/PrioritizedQPSolverqpOASES.h>
+#include <iostream>
 
 namespace prioritized_qp_qpoases{
 
@@ -47,6 +48,9 @@ namespace prioritized_qp_qpoases{
     qpOASES::real_t* ubA = new qpOASES::real_t[inequality_len];
     qpOASES::real_t* lbA = new qpOASES::real_t[inequality_len];
 
+    for (size_t i = 0; i < state_len * state_len; i++) {
+      H[i] = 0.0;
+    }
     for (int k=0; k < this->H_.outerSize(); ++k){
       for (Eigen::SparseMatrix<double,Eigen::ColMajor>::InnerIterator it(this->H_,k); it; ++it){
         H[it.row()*state_len + it.col()] = it.value();
@@ -56,25 +60,30 @@ namespace prioritized_qp_qpoases{
       g[i] = this->g_[i];
     }
     for (size_t i = 0; i < state_len; i++) {
-      lb[i] = -1e20;
-      ub[i] = 1e20;
+      lb[i] = -1e10;
+      ub[i] = 1e10;
+    }
+    for (size_t i = 0; i < state_len * inequality_len; i++) {
+      A[i] = 0.0;
     }
     for (int k=0; k < this->A_.outerSize(); ++k){
       for (Eigen::SparseMatrix<double,Eigen::ColMajor>::InnerIterator it(this->A_,k); it; ++it){
         A[it.row()*state_len + it.col()] = it.value();
       }
     }
-    for (size_t i = 0; i < state_len; i++) {
+    for (size_t i = 0; i < inequality_len; i++) {
       lbA[i] = this->lowerBound_[i];
       ubA[i] = this->upperBound_[i];
     }
 
-    this->example_.setOptions(this->options_);
     qpOASES::returnValue _status;
     int nWSR = 1000;
     if(this->example_.isInitialised() && this->example_.isSolved() && !forceColdStart){
+      this->example_.setOptions(this->options_);
       _status = this->example_.hotstart( H,g,A,lb,ub,lbA,ubA, nWSR);
     }else{
+      this->example_ = qpOASES::SQProblem ( state_len,inequality_len, qpOASES::HST_UNKNOWN);
+      this->example_.setOptions(this->options_);
       _status = this->example_.init( H,g,A,lb,ub,lbA,ubA, nWSR);
     }
     int status = qpOASES::getSimpleStatus(_status);
@@ -91,10 +100,10 @@ namespace prioritized_qp_qpoases{
   }
 
   Eigen::VectorXd Task::getSolution(){
-    qpOASES::real_t* xOpt;
+    qpOASES::real_t* xOpt = new qpOASES::real_t[this->example_.getNV()];
     this->example_.getPrimalSolution( xOpt );
-    Eigen::VectorXd x(this->H_.rows());
-    for(size_t i=0; i<this->H_.rows();i++){
+    Eigen::VectorXd x(this->example_.getNV());
+    for(size_t i=0; i<this->example_.getNV();i++){
       x[i]=xOpt[i];
     }
     return x;
